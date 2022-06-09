b1e992be0e96587596daed613a2bc0bc4a314901
Kaio Delphino <dash.zet@gmail.com>
2022-06-09 12:59:12 -0300

    fix: bug no constraints

--- lib/app/modules/clients/ui/pages/client_search_page.dart
+++ lib/app/modules/clients/ui/pages/client_search_page.dart
@@ -17,7 +17,7 @@ class ClientSearchPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: colors.secondaryContainer,
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.large(
         child: const Icon(Icons.add),
         // TODO: implement client add screen
         onPressed: () => print("Modular.to.pushNamed('/client/add')"),
@@ -37,46 +37,48 @@ class ClientSearchPage extends StatelessWidget {
                 ),
               ),
             ),
-            LayoutBuilder(
-              builder: (_, constraints) => Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: height * 0.05,
-                  horizontal: width * 0.1,
-                ),
-                height: height,
-                width: constraints.maxWidth,
-                // TODO: make needle background image smaller
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage('assets/images/needle.png'),
-                    fit: BoxFit.scaleDown,
+            Expanded(
+              child: LayoutBuilder(
+                builder: (_, constraints) => Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: height * 0.05,
+                    horizontal: width * 0.1,
                   ),
-                  color: colors.primary,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  ),
-                ),
-                child: ScopedBuilder(
-                  store: store,
-                  // TODO: implement error widget or screen
-                  onError: (_, error) => Text("$error"),
-                  onLoading: (_) => const CircularProgressIndicator(),
-                  onState: (_, state) => ListView.separated(
-                    separatorBuilder: (_, __) => SizedBox(
-                      height: height * 0.05,
-                      /* child: Divider(color: colors.secondaryContainer), */
+                  height: constraints.maxHeight,
+                  width: constraints.maxWidth,
+                  // TODO: make needle background image smaller
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/needle.png'),
+                      fit: BoxFit.scaleDown,
                     ),
-                    itemCount: store.state.clientsFiltered.length,
-                    itemBuilder: (_, index) {
-                      final client = store.state.clientsFiltered[index];
+                    color: colors.primary,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: ScopedBuilder(
+                    store: store,
+                    // TODO: implement error widget or screen
+                    onError: (_, error) => Text("$error"),
+                    onLoading: (_) => const CircularProgressIndicator(),
+                    onState: (_, state) => ListView.separated(
+                      separatorBuilder: (_, __) => SizedBox(
+                        height: height * 0.05,
+                        /* child: Divider(color: colors.secondaryContainer), */
+                      ),
+                      itemCount: store.state.clientsFiltered.length,
+                      itemBuilder: (_, index) {
+                        final client = store.state.clientsFiltered[index];
 
-                      return ClientContactWidget(
-                        client: client,
-                        onTap: () =>
-                            Modular.to.pushNamed('./info', arguments: client),
-                      );
-                    },
+                        return ClientContactWidget(
+                          client: client,
+                          onTap: () =>
+                              Modular.to.pushNamed('./info', arguments: client),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ),
