1df7b45ecb054763b3fdd6c5ca61f9763241eeb8
Kaio Delphino <dash.zet@gmail.com>
2022-06-10 16:51:43 -0300

    feature: add page transition

--- lib/app/modules/clients/clients_module.dart
+++ lib/app/modules/clients/clients_module.dart
@@ -4,9 +4,9 @@ import 'package:app_costura/app/modules/clients/domain/usecases/delete_client_us
 import 'package:app_costura/app/modules/clients/domain/usecases/fetch_clients_usecase.dart';
 import 'package:app_costura/app/modules/clients/domain/usecases/update_client_usecase.dart';
 import 'package:app_costura/app/modules/clients/infra/repositories/client_repository.dart';
+import 'package:app_costura/app/modules/clients/ui/pages/client_add_page.dart';
 import 'package:app_costura/app/modules/clients/ui/pages/client_info_page.dart';
 import 'package:app_costura/app/modules/clients/ui/pages/client_search_page.dart';
-import 'package:app_costura/app/modules/clients/ui/pages/client_add_page.dart';
 import 'package:app_costura/app/modules/clients/ui/stores/clients_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive/hive.dart';
@@ -30,8 +30,8 @@ class ClientsModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const ClientSearchPage()),
-        ChildRoute('/info', child: (context, args) => ClientInfoPage(client: args.data)),
-        ChildRoute('/add', child: (context, args) => const ClientAddPage()),
+        ChildRoute('/', child: (context, args) => const ClientSearchPage(), transition: TransitionType.fadeIn),
+        ChildRoute('/info', child: (context, args) => ClientInfoPage(client: args.data), transition: TransitionType.downToUp),
+        ChildRoute('/add', child: (context, args) => const ClientAddPage(), transition: TransitionType.downToUp),
       ];
 }
